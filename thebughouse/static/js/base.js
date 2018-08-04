9184b007fae4e429076dd1e69c45799b6d22b68e
Camen Piho <camen@getfreebird.com>
2018-08-04 12:23:00 -0400

    indented form

--- thebughouse/static/js/base.js
+++ thebughouse/static/js/base.js
@@ -1,4 +1,4 @@
-function loginBodyHandler(event, trigger, parent, closeFunction) {
+function closeHandler(event, trigger, parent, closeFunction) {
     var triggerElement = document.getElementById(trigger);
     var parentElement = document.getElementById(parent);
     var childElements = parentElement.getElementsByTagName('*');
@@ -22,7 +22,7 @@ function openSideNav() {
     document.getElementById("side-navigation").style.transform = "translate(0, 0)";
     document.body.style.background = "rgba(0, 0, 0, 0.54)";
     document.body.addEventListener("click", _sideNavCloseListener = function () {
-        loginBodyHandler(event, "side-navigation-toggle", "side-navigation", closeSideNav)
+        closeHandler(event, "side-navigation-toggle", "side-navigation", closeSideNav)
     });
 }
 
@@ -30,4 +30,12 @@ function closeSideNav() {
     document.getElementById("side-navigation").style.transform = "translate(-70vw, 0)";
     document.body.style.background = "#f1f1f1";
     document.body.removeEventListener("click", _sideNavCloseListener);
+}
+
+function openElaboration(elaboration_id) {
+    console.log(elaboration_id);
+    document.getElementById(elaboration_id).classList.toggle("show");
+    // document.body.addEventListener("click", _elaborationCloseListener = function() {
+    //     closeHandler(event, "")
+    // })
 }
